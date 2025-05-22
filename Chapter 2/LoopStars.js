7dc883042af7223b0e0452cf332d5575b76618ca
theseven7h <jamestauri123@gmail.com>
2025-05-22 07:04:44 +0100

    Pattern redone

--- Chapter 2/LoopStars.js
+++ Chapter 2/LoopStars.js
@@ -1,6 +1,5 @@
+let hash = "";
 for (let count = 1; count <= 7; count++) {
-	for (let count2 = 1; count2 <= count; count2++) {
-		process.stdout.write("#");
-	}
-	console.log();
+	hash += "#"
+	console.log(hash);
 }
